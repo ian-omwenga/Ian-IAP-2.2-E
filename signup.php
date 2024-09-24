2b95d7dfe84ef6824cbb2f5c818d122a2948b152
Ian Omwenga <ian.omwenga@strathmore.edu>
2024-09-24 22:38:55 +0300

    Importing Bootstrap

--- signup.php
+++ signup.php
@@ -61,6 +61,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <title>Sign-Up Form</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
     <div class="container">
--- structure/menus.php
+++ structure/menus.php
@@ -5,6 +5,7 @@
             <div class="topnav">
                 <a href="">Home</a>
                 <a href="aboutus.php">About Us</a>
+                <a href="signup.php">Sign Up</a>
                 <a href="">Our Projects</a>
                 <a href="">Our Portfolio</a>
                 <a href="">Blog</a>
