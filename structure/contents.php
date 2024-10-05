563f835f9f943393117ffcec40a1e7e36a83f9a5
Ian Omwenga <ian.omwenga@strathmore.edu>
2024-10-05 14:32:03 +0300

    Add image folder

--- structure/contents.php
+++ structure/contents.php
@@ -6,7 +6,7 @@ class contents{
         <div class="row">
             <div class="content">
 
-            <img src="C:\Users\gmoma\Downloads\yankrukov.jpg" alt="">
+            <img src="C:\xampp\htdocs\fnc for php\images\batman.jpeg" alt="batman">
                 <h1>Main Content</h1>
                 <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
