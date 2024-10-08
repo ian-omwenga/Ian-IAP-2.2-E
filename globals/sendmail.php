98b3f2e3a0f75db6dbe2c45fba8aafdb5d43acec
Ian Omwenga <ian.omwenga@strathmore.edu>
2024-10-09 00:21:27 +0300

    Configuring phpmailer

--- globals/sendmail.php
+++ globals/sendmail.php
@@ -1,46 +1,46 @@
 <?php
-//Import PHPMailer classes 
+// Import PHPMailer classes 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-class sendmail{
+class sendmail {
+    public function sendmail($mailMsg) {
+        require 'C:\xampp\htdocs\fnc for php\vendor\autoload.php';
+        $mail = new PHPMailer(true);
 
-//Top of page
-public function sendmail($mailMsg){
+        try {
+            $mail->SMTPDebug = 2; // Debug info
+            $mail->isSMTP();
+            $mail->Host       = 'smtp.gmail.com';
+            $mail->SMTPAuth   = true;
+            $mail->Username   = 'omwengaim@gmail.com'; // Your Gmail address
+            $mail->Password   = 'Ged!on77'; // Your email password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+            $mail->Port       = 465;
 
-    //Load Composer's autoloader
-    require 'plugins/PHPMailer/vendor/autoload.php';
+            $mail->setFrom('ics@gmail.com', 'ICS 2024');
+            $mail->addAddress($mailMsg['to_email'], $mailMsg['to_name']);
 
-  
-    $mail = new PHPMailer(true);
+            $mail->isHTML(true);
+            $mail->Subject = $mailMsg['subject'];
+            $mail->Body    = nl2br($mailMsg['message']);
 
-    try {
-      
-        $mail->SMTPDebug = 0;                      
-        $mail->isSMTP();                                            
-        $mail->Host       = 'smtp.gmail.com';                     
-        $mail->SMTPAuth   = true;                                   
-        $mail->Username   = 'gmomanyian@gmail.com';                    
-        $mail->Password   = '12345678';                               
-        $mail->Password   = '';                               
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            
-        $mail->Port       = 465;                                    
-
-        //Recipients
-        $mail->setFrom('ics@gmail.com', 'ICS 2024');
-        $mail->addAddress($mailMsg['to_email'], $mailMsg['to_name']);     
-
-        //Content
-        $mail->isHTML(true);                                  
-        $mail->Subject = $mailMsg['subject'];
-        $mail->Body    = nl2br($mailMsg['message']);
-
-        $mail->send();
-
-        echo 'Message has been sent';
-
-    } catch (Exception $e) {
-        die("Message could not be sent. Mailer Error: {$mail->ErrorInfo}");
+            $mail->send();
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
     }
 }
+
+// Call the sendmail function after the class definition
+$mailMsg = [
+    'to_email' => 'omwengaim@gmail.com',
+    'to_name'  => 'IANNO',
+    'subject'  => 'MY SUBJECT IS brooo',
+    'message'  => 'What is up brooo'
+];
+
+$mailer = new sendmail();
+$mailer->sendmail($mailMsg);
